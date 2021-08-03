e65dd63ad7a482a1b7c821e5afc7476e3f141d56
Ananias Raphael <ananias.nobrega@gmail.com>
2021-08-02 22:11:45 -0300

    3.9 finalizando o capitulo

--- client/app/controller/NegociacaoController.js
+++ client/app/controller/NegociacaoController.js
@@ -13,16 +13,14 @@ class NegociacaoController{
         let	data = new Date(
             ...this._inputData.value
             .split('-')
-            .map(function (item, indice) {
-                return	item	-	indice	%	2;
-            })
+            .map((item,	indice)	=>	item - indice	%	2)
             );
 
-        // let negociacao = new Negociacao(
-        //     this._inputData.value,
-        //     parseInt(this._inputQuantidade.value),
-        //     parseFloat(this._inputValor.value)
-        // );     
+        let negociacao = new Negociacao(
+            data,
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );     
         
         console.log(negociacao);
     
