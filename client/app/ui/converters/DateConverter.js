515f1a268ac6f64820079168f233b496f984e10c
Ananias Raphael <ananias.nobrega@gmail.com>
2021-08-11 12:05:46 -0300

    4.4 boas praticas

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -8,6 +8,11 @@ class DataConverter{
         return	`${date.getData()}/${date.getMonth()+1}/${date.getFullYear()}`;
     }
     static toDate(text){
+
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(date)){
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+        }
+
         return new Date(...text.split('-').map((item, index) => item - index % 2));
     }
 }
