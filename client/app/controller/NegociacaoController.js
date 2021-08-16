1652500a57c38daffe7bd1a38d830bab9248d669
Ananias Raphael <ananias.nobrega@gmail.com>
2021-08-16 00:25:30 -0300

    5.2 controller modificacoes

--- client/app/controller/NegociacaoController.js
+++ client/app/controller/NegociacaoController.js
@@ -11,17 +11,26 @@ class NegociacaoController{
     adcionar(event){
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        let negociacao = this._createNegociacao();     
+    
+        this._negociacoes.adciona(negociacao);
+        this._cleanForm();
+        console.log(this._negociacoes.toArray());
+    }
+
+    _cleanForm(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    _createNegociacao(){
+        return new Negociacao(
             DataConverter.toDate(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
-        );     
-        
-        this._negociacoes.adciona(negociacao);
-
-        console.log(this._negociacoes.toArray());
-    
+        );
     }
-
 
 }
