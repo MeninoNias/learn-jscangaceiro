2b1c8938c06cd7c2093d156936f312d2c21c3e50
Ananias Raphael <ananias.nobrega@gmail.com>
2021-08-17 02:00:01 -0300

    6.2 view implements

--- client/app/controller/NegociacaoController.js
+++ client/app/controller/NegociacaoController.js
@@ -6,6 +6,9 @@ class NegociacaoController{
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+
+        this._negociacoesView.update();
     }
 
     adcionar(event){
